0a0dfc7956a616290741cc27f0f9dd83867e4735
LNZRK-HTC <71545492+LNZRK-HTC@users.noreply.github.com>
2021-03-26 15:56:48 +0800

    Add files via upload

---
+++ week03/week03-6.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b && b>c)printf("%d\n",a-c);
+	else if(a>c && c>b)printf("%d\n",a-b);
+	else if(b>a && a>c)printf("%d\n",b-c);
+	else if(b>c && c>a)printf("%d\n",b-a);
+	else if(c>a && a>b)printf("%d\n",c-b);
+	else if(c>b && b>a)printf("%d\n",c-a);
+}
